d0f4b58e8b09dcd993da9a19b80ea65a66e6e198
Bridier Erwan <e.bridier@hotmail.com>
2020-07-26 23:44:44 +0200

    fonction next com

--- billet.php
+++ billet.php
@@ -2,6 +2,6 @@
     <h3><?php echo $donnees['titre'];?> publié le <?php echo $donnees['date'];?> </h3>
     <p>
         <?php echo $donnees['contenue'];?><br/>
-        <a href="commentaires.php?id_billet=<?php echo $donnees['id'];?>">Commentaires</a>
+        <a href="commentaires.php?id_billet=<?php echo $donnees['id'];?>&amp;page=1">Commentaires</a>
     </p>
 </div>
